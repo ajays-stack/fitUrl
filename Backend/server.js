144f2209e414900cfd863b9c3a6a2662dfbf59fc
ajay <singhajay9968@gmail.com>
2025-07-19 01:56:34 +0530

    new commit

--- Backend/server.js
+++ Backend/server.js
@@ -1,15 +1,17 @@
 import express from 'express'
 import connect from './connnection.js';
 import dotenv from 'dotenv';
+dotenv.config()
 import urlroute from "./routes/url.js";
 import cors from 'cors'
 import userRouter from './routes/user.js';
 const app=express();
 app.use(express.json());
+
 app.use(cors({
     origin:process.env.FRONTEND_URL
 }));
-dotenv.config();
+
 
 app.use('/url',urlroute)
 app.use('/user',userRouter)
